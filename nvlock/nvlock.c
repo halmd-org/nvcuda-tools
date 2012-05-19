10114906b00426012c85995cd9a8add26d59e4da
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-05-19 00:41:31 -0400

    nvlock: support CUDA runtime version 3.1 to 4.2
    Support use of nvlock with *unmodified* program using CUDA runtime library.
    This has been tested with CUDA driver 4.2 and CUDA runtime 3.1/3.2/4.0/4.2.
    
    Remove program nvlock, as LD_PRELOAD does not work with CUDA 3.x
    since the CUDA runtime library uses dlopen() to load the driver.
    To use nvlock, prepend its lib/ directory to LD_LIBRARY_PATH.
    
    For CUDA 3.1 and 3.2, monitor cuCtxCreate, cuCtxAttach and cuCtxDetach
    to create or destroy context, along with lock or unlock of device.
    
    For CUDA ≥ 4.0, monitor cuCtxSetCurrent, which is used both to set and
    unset the current context. CUDA ≥ 4.0 does not use cuCtxCreate, and
    instead seems to fills the data structure CUcontext internally.
    
    Test if libnvlock.so is loadable in nvlock program.
    
    To trace execution, configure CMake in RelWithDebInfo or Debug mode.

--- nvlock/nvlock.c
+++ nvlock/nvlock.c
@@ -1,7 +1,7 @@
 /*
- * nvlock - Exclusively lock an unused NVIDIA device
+ * nvlock - Exclusively lock an unused CUDA device
  *
- * Copyright © 2008-2012  Peter Colberg
+ * Copyright © 2008-2012 Peter Colberg
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,10 +17,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* enable GNU extension RTLD_NEXT */
+#define _GNU_SOURCE
+
+/* always enable assertions */
+#ifdef NDEBUG
+# undef NDEBUG
+# include <assert.h>
+# define NDEBUG
+#else
+# include <assert.h>
+#endif
+
 #include <cuda.h>
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/file.h>
 #include <unistd.h>
 
@@ -28,130 +41,223 @@
 #define xstr(s) str(s)
 #define str(s) #s
 
-#define NVIDIA_DEVICE_FILENAME "/dev/nvidia%d"
+/* path to nvidia devices */
+#ifndef NVLOCK_DEVICE_PATH
+# define NVLOCK_DEVICE_PATH "/dev/nvidia%d"
+#endif
 
-static CUresult (*_cuCtxCreate)(CUcontext *, unsigned int, CUdevice) = 0;
-static CUresult (*_cuCtxPopCurrent)(CUcontext *) = 0;
-static CUresult (*_cuCtxPushCurrent)(CUcontext) = 0;
-static int fd = -1;
-
+/* print error message to stderr */
 #define LOG_ERROR(fmt, args...) fprintf(stderr, "nvlock: " fmt "\n", ## args)
 
-CUresult cuCtxCreate(CUcontext *pctx, unsigned int flags, CUdevice dev)
+/* print debug message to stderr */
+#ifdef NDEBUG
+# define LOG_DEBUG(fmt, args...)
+#else
+# define LOG_DEBUG(fmt, args...) fprintf(stderr, "nvlock: " fmt "\n", ## args)
+#endif
+
+/* file descriptor of lock on nvidia device */
+static int fd = -1;
+/* usage count of current CUDA context */
+static int use = 0;
+
+/* lock CUDA device */
+static CUresult lock_device(CUdevice dev)
 {
-    void *handle;
-    char fn[16];
+    char fn[256];
     char *env, *endptr;
     long int val;
-
-    /* open dynamic library and load real function symbol */
-    if (!_cuCtxCreate) {
-        handle = dlopen("libcuda.so", RTLD_GLOBAL | RTLD_NOW);
-        if (!handle) {
-            LOG_ERROR("failed to load libcuda.so");
-            return CUDA_ERROR_UNKNOWN;
-        }
-        /* stringify: CUDA >= 3.2 defines part of its API with macros */
-        _cuCtxCreate = dlsym(handle, xstr(cuCtxCreate));
-        if (!_cuCtxCreate) {
-            LOG_ERROR("failed to load symbol " xstr(cuCtxCreate));
-            return CUDA_ERROR_UNKNOWN;
-        }
-        if (dlclose(handle)) {
-            LOG_ERROR("failed to unload libcuda.so");
-            return CUDA_ERROR_UNKNOWN;
-        }
-    }
+    int result;
 
     /* if environment variable CUDA_DEVICE is set, use integer value as allowed device */
     env = getenv("CUDA_DEVICE");
-    if (NULL != env && '\0' != *env) {
+    if (env != NULL && *env != '\0') {
         val = strtol(env, &endptr, 10);
-        if ('\0' != *endptr || val < 0) {
-            LOG_ERROR("invalid environment variable CUDA_DEVICE: %s", env);
+        if (*endptr != '\0' || val < 0) {
+            LOG_ERROR("invalid CUDA_DEVICE environment variable: %s", env);
             return CUDA_ERROR_UNKNOWN;
         }
-        if (val != dev) {
+        if (dev != val) {
             return CUDA_ERROR_UNKNOWN;
         }
     }
 
     /* lock NVIDIA device file with non-blocking request */
-    snprintf(fn, sizeof(fn), NVIDIA_DEVICE_FILENAME, dev);
-    if (-1 != fd) {
-        close(fd);
-        fd = -1;
-    }
-    if (-1 == (fd = open(fn, O_RDWR))) {
+    snprintf(fn, sizeof(fn), NVLOCK_DEVICE_PATH, dev);
+    assert(fd == -1);
+    fd = open(fn, O_RDWR);
+    if (fd == -1) {
         LOG_ERROR("failed to open CUDA device in read-write mode: %s", fn);
         return CUDA_ERROR_UNKNOWN;
     }
-    if (-1 == flock(fd, LOCK_EX | LOCK_NB)) {
+    result = flock(fd, LOCK_EX | LOCK_NB);
+    if (result == -1) {
         close(fd);
         fd = -1;
         return CUDA_ERROR_UNKNOWN;
     }
-
-    /* create CUDA context */
-    return _cuCtxCreate(pctx, flags, dev);
+    LOG_DEBUG("lock device %i", dev);
+    return CUDA_SUCCESS;
 }
 
-CUresult cuCtxPopCurrent(CUcontext *pctx)
+/* unlock CUDA device */
+static void unlock_device()
 {
-    void *handle;
-
-    /* open dynamic library and load real function symbol */
-    if (!_cuCtxPopCurrent) {
-        handle = dlopen("libcuda.so", RTLD_GLOBAL | RTLD_NOW);
-        if (!handle) {
-            LOG_ERROR("failed to load libcuda.so");
-            return CUDA_ERROR_UNKNOWN;
-        }
-        /* stringify: CUDA >= 3.2 defines part of its API with macros */
-        _cuCtxPopCurrent = dlsym(handle, xstr(cuCtxPopCurrent));
-        if (!_cuCtxPopCurrent) {
-            LOG_ERROR("failed to load symbol " xstr(cuCtxPopCurrent));
-            return CUDA_ERROR_UNKNOWN;
-        }
-        if (dlclose(handle)) {
-            LOG_ERROR("failed to unload libcuda.so");
-            return CUDA_ERROR_UNKNOWN;
-        }
-    }
-
-    /* unlock the device file */
-    flock(fd, LOCK_UN);
-
-    /* pop current context from CUDA context stack */
-    return _cuCtxPopCurrent(pctx);
+    LOG_DEBUG("unlock device");
+    assert(fd != -1);
+    close(fd);
+    fd = -1;
 }
 
-CUresult cuCtxPushCurrent(CUcontext ctx)
-{
-    void *handle;
+#if CUDA_VERSION >= 4000
 
-    /* open dynamic library and load real function symbol */
-    if (!_cuCtxPushCurrent) {
-        handle = dlopen("libcuda.so", RTLD_GLOBAL | RTLD_NOW);
-        if (!handle) {
-            LOG_ERROR("failed to load libcuda.so");
-            return CUDA_ERROR_UNKNOWN;
+CUresult CUDAAPI cuCtxSetCurrent(CUcontext ctx)
+{
+    CUresult (*cuCtxSetCurrent)(CUcontext);
+    CUresult result;
+    CUdevice dev;
+
+    LOG_DEBUG("cuCtxSetCurrent(%p)", ctx);
+    cuCtxSetCurrent = dlsym(RTLD_NEXT, xstr(cuCtxSetCurrent));
+    if (cuCtxSetCurrent == NULL) {
+        LOG_ERROR("failed to resolve symbol " xstr(cuCtxSetCurrent));
+        return CUDA_ERROR_UNKNOWN;
+    }
+    result = cuCtxSetCurrent(ctx);
+    if (result != CUDA_SUCCESS) {
+        return result;
+    }
+    /* if context is NULL, decrement usage count */
+    if (ctx == NULL) {
+        assert(use > 0);
+        --use;
+    }
+    /** if usage count is 0, lock or unlock device */
+    if (use == 0) {
+        if (ctx == NULL) {
+            unlock_device();
         }
-        /* stringify: CUDA >= 3.2 defines part of its API with macros */
-        _cuCtxPushCurrent = dlsym(handle, xstr(cuCtxPushCurrent));
-        if (!_cuCtxPushCurrent) {
-            LOG_ERROR("failed to load symbol " xstr(cuCtxPopCurrent));
-            return CUDA_ERROR_UNKNOWN;
-        }
-        if (dlclose(handle)) {
-            LOG_ERROR("failed to unload libcuda.so");
-            return CUDA_ERROR_UNKNOWN;
+        else {
+            result = cuCtxGetDevice(&dev);
+            if (result != CUDA_SUCCESS) {
+                cuCtxSetCurrent(0);
+                return result;
+            }
+            result = lock_device(dev);
+            if (result != CUDA_SUCCESS) {
+                cuCtxSetCurrent(0);
+                return result;
+            }
         }
     }
+    /* if context is not NULL, increment usage count */
+    if (ctx != NULL) {
+        ++use;
+    }
+    return CUDA_SUCCESS;
+}
 
-    /* lock the device file with blocking request */
-    flock(fd, LOCK_EX);
+#endif /* CUDA_VERSION >= 4000 */
 
-    /* push floating context onto CUDA context stack */
-    return _cuCtxPushCurrent(ctx);
+CUresult CUDAAPI cuCtxCreate(CUcontext *pctx, unsigned int flags, CUdevice dev)
+{
+    CUresult (*cuCtxCreate)(CUcontext *, unsigned int, CUdevice);
+    CUresult result;
+
+    cuCtxCreate = dlsym(RTLD_NEXT, xstr(cuCtxCreate));
+    if (cuCtxCreate == NULL) {
+        LOG_ERROR("failed to resolve symbol " xstr(cuCtxCreate));
+        return CUDA_ERROR_UNKNOWN;
+    }
+    result = lock_device(dev);
+    if (result != CUDA_SUCCESS) {
+        return result;
+    }
+    result = cuCtxCreate(pctx, flags, dev);
+    if (result != CUDA_SUCCESS) {
+        unlock_device();
+        return result;
+    }
+    LOG_DEBUG("cuCtxCreate(%p, %u, %i)", *pctx, flags, dev);
+    assert(use == 0);
+    ++use;
+    return CUDA_SUCCESS;
+}
+
+#if CUDA_VERSION >= 3020
+
+# undef cuCtxCreate
+
+CUresult CUDAAPI cuCtxCreate(CUcontext *pctx, unsigned int flags, CUdevice dev)
+{
+    CUresult (*cuCtxCreate)(CUcontext *, unsigned int, CUdevice);
+    CUresult result;
+
+    cuCtxCreate = dlsym(RTLD_NEXT, xstr(cuCtxCreate));
+    if (cuCtxCreate == NULL) {
+        LOG_ERROR("failed to resolve symbol " xstr(cuCtxCreate));
+        return CUDA_ERROR_UNKNOWN;
+    }
+    result = lock_device(dev);
+    if (result != CUDA_SUCCESS) {
+        return result;
+    }
+    result = cuCtxCreate(pctx, flags, dev);
+    if (result != CUDA_SUCCESS) {
+        unlock_device();
+        return result;
+    }
+    LOG_DEBUG("cuCtxCreate(%p, %u, %i)", *pctx, flags, dev);
+    assert(use == 0);
+    ++use;
+    return CUDA_SUCCESS;
+}
+
+#endif /* CUDA_VERSION >= 3020 */
+
+CUresult CUDAAPI cuCtxAttach(CUcontext *pctx, unsigned int flags)
+{
+    CUresult (*cuCtxAttach)(CUcontext *, unsigned int);
+    CUresult result;
+
+    cuCtxAttach = dlsym(RTLD_NEXT, xstr(cuCtxAttach));
+    if (cuCtxAttach == NULL) {
+        LOG_ERROR("failed to resolve symbol " xstr(cuCtxAttach));
+        return CUDA_ERROR_UNKNOWN;
+    }
+    result = cuCtxAttach(pctx, flags);
+    if (result != CUDA_SUCCESS) {
+        return result;
+    }
+    LOG_DEBUG("cuCtxAttach(%p, %u)", *pctx, flags);
+    if (*pctx == NULL) {
+        assert(use == 0);
+    }
+    else {
+        ++use;
+    }
+    return CUDA_SUCCESS;
+}
+
+CUresult CUDAAPI cuCtxDetach(CUcontext ctx)
+{
+    CUresult (*cuCtxDetach)(CUcontext);
+    CUresult result;
+
+    cuCtxDetach = dlsym(RTLD_NEXT, xstr(cuCtxDetach));
+    if (cuCtxDetach == NULL) {
+        LOG_ERROR("failed to resolve symbol " xstr(cuCtxDetach));
+        return CUDA_ERROR_UNKNOWN;
+    }
+    result = cuCtxDetach(ctx);
+    if (result != CUDA_SUCCESS) {
+        return result;
+    }
+    LOG_DEBUG("cuCtxDetach(%p)", ctx);
+    assert(use > 0);
+    --use;
+    if (use == 0) {
+        unlock_device();
+    }
+    return result;
 }
